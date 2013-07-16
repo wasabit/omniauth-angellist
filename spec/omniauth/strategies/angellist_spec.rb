e23e859f6ee504495cf677d9db84d81b9712fc6d
Bob Aman <bob@sporkmonger.com>
2013-07-15 18:35:58 -0700

    Rearranged mocks to get specs to pass.

--- spec/omniauth/strategies/angellist_spec.rb
+++ spec/omniauth/strategies/angellist_spec.rb
@@ -7,6 +7,39 @@ describe OmniAuth::Strategies::AngelList do
     @request.stub(:params) { {} }
     @client_id = '123'
     @client_secret = 'afalsf'
+    @raw_info = {
+      'name' => 'Sebastian Rabuini',
+      'email' => 'sebas@wasabit.com.ar',
+      'bio' => 'Sebas',
+      'blog_url' => 'sebas_blog',
+      'online_bio_url' => 'http://wasabitlabs.com',
+      'twitter_url' => 'http://twitter.com/#!/sebasr',
+      'facebook_url' => 'http://www.facebook.com/sebastian.rabuini',
+      'linkedin_url' => 'http://www.linkedin.com/in/srabuini',
+      'follower_count' => 6,
+      'investor' => false,
+      'locations' => [
+        {'id' => 1963, 'tag_type' => 'LocationTag', 'name' => 'buenos aires',
+          'display_name' => 'Buenos Aires',
+          'angellist_url' => 'https://angel.co/buenos-aires'}
+      ],
+      'roles' => [
+        {'id' => 14726, 'tag_type' => 'RoleTag', 'name' => 'developer',
+          'display_name' => 'Developer',
+          'angellist_url' => 'https://angel.co/developer'},
+        {'id' => 14725, 'tag_type' => 'RoleTag', 'name' => 'entrepreneur',
+          'display_name' => 'Entrepreneur',
+          'angellist_url' => 'https://angel.co/entrepreneur-1'}
+      ],
+      'skills' => [
+        {"id" => 82532, "tag_type" => "SkillTag", "name" => "ruby on rails",
+          "display_name" => "Ruby on Rails",
+          "angellist_url" => "https://angel.co/ruby-on-rails-1"}
+      ],
+      'scopes' => ["email","comment","message","talent"],
+      'angellist_url' => 'https://angel.co/sebasr',
+      'image' => 'https://s3.amazonaws.com/photos.angel.co/users/90585-medium_jpg?1327684569'
+    }
   end
   
   subject do
@@ -34,39 +67,6 @@ describe OmniAuth::Strategies::AngelList do
 
   describe '#info' do
     before :each do
-      @raw_info = {
-        'name' => 'Sebastian Rabuini',
-        'email' => 'sebas@wasabit.com.ar',
-        'bio' => 'Sebas',
-        'blog_url' => 'sebas_blog',
-        'online_bio_url' => 'http://wasabitlabs.com',
-        'twitter_url' => 'http://twitter.com/#!/sebasr',
-        'facebook_url' => 'http://www.facebook.com/sebastian.rabuini',
-        'linkedin_url' => 'http://www.linkedin.com/in/srabuini',
-        'follower_count' => 6,
-        'investor' => false,
-        'locations' => [
-          {'id' => 1963, 'tag_type' => 'LocationTag', 'name' => 'buenos aires',
-            'display_name' => 'Buenos Aires',
-            'angellist_url' => 'https://angel.co/buenos-aires'}
-        ],
-        'roles' => [
-          {'id' => 14726, 'tag_type' => 'RoleTag', 'name' => 'developer',
-            'display_name' => 'Developer',
-            'angellist_url' => 'https://angel.co/developer'},
-          {'id' => 14725, 'tag_type' => 'RoleTag', 'name' => 'entrepreneur',
-            'display_name' => 'Entrepreneur',
-            'angellist_url' => 'https://angel.co/entrepreneur-1'}
-        ],
-        'skills' => [
-          {"id" => 82532, "tag_type" => "SkillTag", "name" => "ruby on rails",
-            "display_name" => "Ruby on Rails",
-            "angellist_url" => "https://angel.co/ruby-on-rails-1"}
-        ],
-        'scopes' => ["email","comment","message","talent"],
-        'angellist_url' => 'https://angel.co/sebasr',
-        'image' => 'https://s3.amazonaws.com/photos.angel.co/users/90585-medium_jpg?1327684569'
-      }
       subject.stub(:raw_info) { @raw_info }
     end
     
@@ -85,10 +85,6 @@ describe OmniAuth::Strategies::AngelList do
 
       it "return the email" do
         subject.info['email'].should eq('sebas@wasabit.com.ar')
-      end
-
-      it "return scopes" do
-        subject.credentials['scope'].should eq("email comment message talent")
       end
 
       it "return skills" do
@@ -110,11 +106,12 @@ describe OmniAuth::Strategies::AngelList do
   describe '#credentials' do
     before :each do
       @access_token = double('OAuth2::AccessToken')
-      @access_token.stub(:token)
+      @access_token.stub(:token) { '123' } # Token is always required
       @access_token.stub(:expires?)
       @access_token.stub(:expires_at)
       @access_token.stub(:refresh_token)
       subject.stub(:access_token) { @access_token }
+      subject.stub(:raw_info) { @raw_info }
     end
     
     it 'returns a Hash' do
@@ -122,8 +119,11 @@ describe OmniAuth::Strategies::AngelList do
     end
     
     it 'returns the token' do
-      @access_token.stub(:token) { '123' }
       subject.credentials['token'].should eq('123')
+    end
+
+    it "return scopes" do
+      subject.credentials['scope'].should eq("email comment message talent")
     end
     
     it 'returns the expiry status' do
